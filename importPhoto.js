d2590231b07b12301dd8cce9f676a8c35737e067
ukoloff <ukoloff@gmail.com>
2013-03-21 15:31:27 +0600

    Connect to AD

--- importPhoto.js
+++ importPhoto.js
@@ -22,12 +22,32 @@ function goDir()
 
 function goSQL()
 {
+ WScript.Echo("Connecting to MS SQL...");
+
  var SQL=new ActiveXObject("ADODB.Connection");
  SQL.Provider='SQLOLEDB';
  SQL.Open("Integrated Security=SSPI;Data Source="+$.Dir.Server);
  SQL.DefaultDatabase='['+$.Dir.DB+']';
  $.SQL=new ActiveXObject("ADODB.Command");
  $.SQL.ActiveConnection=SQL;
+}
+
+function goAD()
+{
+ WScript.Echo("Connecting to Active Directory...");
+
+ $.AD={Domain: (new ActiveXObject("ADSystemInfo")).DomainShortName};
+ $.AD.rootDSE=GetObject("LDAP://rootDSE");
+ $.AD.baseDN=$.AD.rootDSE.Get('rootDomainNamingContext');
+
+ $.AD.h=new ActiveXObject("ADODB.Connection");
+ $.AD.h.Provider = "ADsDSOObject";
+ $.AD.h.Open("Active Directory Provider");
+
+ $.AD.cmd=new ActiveXObject("ADODB.Command");
+ $.AD.cmd.ActiveConnection=$.AD.h;
+ $.AD.cmd.Properties("Page Size")=1000;
+ $.AD.cmd.Properties("Searchscope")=2; // ADS_SCOPE_SUBTREE
 }
 
 // Выделить кусочек текста из исходного кода
