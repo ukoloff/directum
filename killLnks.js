9ff0b6ae352de9d370046151552965a78bb054d6
ukoloff <ukoloff@gmail.com>
2013-04-01 16:30:14 +0600

    Get user profiles

--- killLnks.js
+++ killLnks.js
@@ -2,13 +2,23 @@
 // Найти все ярлыки Directum и уничтожить
 //
 
+var Key="SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
+
 var R=GetObject("winmgmts:\\\\.\\root\\default:StdRegProv");
 var M=R.Methods_.Item("EnumKey");
 var PI=M.InParameters.SpawnInstance_(); 
 PI.hDefKey=0x80000002;	// HKLM
-PI.sSubKeyName="SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
-var PO=VBArray(R.ExecMethod_(M.Name, PI).sNames).toArray();
+PI.sSubKeyName=Key;
+var Us=VBArray(R.ExecMethod_(M.Name, PI).sNames).toArray();
 
-WScript.Echo(PO.join('\n'));
+var Sh=new ActiveXObject("WScript.Shell");
+
+for(var i in Us)
+{
+ var S=Us[i];
+ var Path=Sh.RegRead('HKLM\\'+Key+'\\'+S+'\\ProfileImagePath');
+ Path=Sh.ExpandEnvironmentStrings(Path);
+ WScript.Echo(Path);
+}
 
 //--[EOF]------------------------------------------------------------
